42aa35f7bdd3bc5d1dba32acc49a3e4de4ff362f
Ashley Weber <aweber333@yahoo.com>
2018-08-18 12:50:25 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,32 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+var conection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else {
+  conection = mysql.createConnection({
+
+// var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
   user: "root",
   password: "root",
   database: "burgers_db"
-});
+// });
+  });
+};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+conection.connect();
+module.exports = connection;
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
